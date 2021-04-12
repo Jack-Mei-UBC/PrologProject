bb3a23c538773a648069b7b40ccac22602aba8d8
kmr-source <59981595+kmr-source@users.noreply.github.com>
2021-04-11 23:43:29 -0700

    Add files via upload
    update def for atoms

--- main.pl
+++ main.pl
@@ -1,5 +1,8 @@
+% import moves and test
 :- [moves].
 :- [test].
+
+% initates the board piece placements initate2(Board)
 initiate2(B) :- B = [piece(w,rook,1,1),piece(w,knight,2,1),piece(w,bishop,3,1),piece(w,queen,4,1),piece(w,king,5,1),piece(w,bishop,6,1),piece(w,knight,7,1),piece(w,rook,8,1),
 piece(w, pawn,1, 2),piece(w, pawn,2, 2),piece(w, pawn, 3, 2),piece(w, pawn, 4, 2),piece(w, pawn, 5, 2),piece(w, pawn, 6, 2),piece(w, pawn, 7,2),piece(w, pawn, 8, 2),
 piece("-","-",1,3),piece("-","-",2,3),piece("-","-",3,3),piece("-","-",4,3),piece("-","-",5,3),piece("-","-",6,3),piece("-","-",7,3),piece("-","-",8,3),
@@ -8,48 +11,47 @@ piece("-","-",1,5),piece("-","-",2,5),piece("-","-",3,5),piece("-","-",4,5),piec
 piece("-","-",1,6),piece("-","-",2,6),piece("-","-",3,6),piece("-","-",4,6),piece("-","-",5,6),piece("-","-",6,6),piece("-","-",7,6),piece("-","-",8,6),
 piece(b, pawn,1, 7),piece(b, pawn,2, 7),piece(b, pawn, 3, 7),piece(b, pawn, 4, 7),piece(b, pawn, 5, 7),piece(b, pawn, 6, 7),piece(b, pawn, 7,7),piece(b, pawn, 8, 7),
 piece(b,rook,1,8),piece(b,knight,2,8),piece(b,bishop,3,8),piece(b,queen,4,8),piece(b,king,5,8),piece(b,bishop,6,8),piece(b,knight,7,8),piece(b,rook,8,8)].
-% white piece
+
+% Putting "W" in front of the pieces that are white on the Board 
 pprint(piece(w,Type,_,_)) :- 
     letter(Type,Letter),
     write("[W"),
     write(Letter),
     write("]").
-% black piece
+    
+% Putting "B" in front of the black pieces that are on the Board 
 pprint(piece(b,Type,_,_)) :- 
     letter(Type,Letter),
     write("[B"),
     write(Letter),
     write("]").
-% empty space
+    
+% Putting [--] for empty spaces on the Board 
 pprint(piece("-",_,_,_)) :-
     write("[--]").
 
+
+% Writes the Pieces on the Board recursive calls till all pieces are written 
 writeboard([piece(C,Type,1,Y)|T]) :-
     write("\n"),
     write(Y),
     write("  "),
     pprint(piece(C,Type,1,Y)),
     writeboard(T).
+  
 writeboard([X|T]) :-
     pprint(X),
     writeboard(T).
     
     
-% writeBoard([piece(C,T,X,Y)])
+% writeBoard([piece(C,T,X,Y)]), C for color, T for Type , X for X coord Y for Y coord 
 writeboard([piece(C,T,X,Y)]):-
     write("\n   a   b   c   d   e   f   g   h").
-test:- initiate2(B),writeboard(B).
+
+%test:- initiate2(B),writeboard(B).
 
 
-convertRow("a",1).
-convertRow("b",2).
-convertRow("c",3).
-convertRow("d",4).
-convertRow("e",5).
-convertRow("f",6).
-convertRow("g",7).
-convertRow("h",8).
-
+% return true if  the cooresponding letter/ character exist 
 letter(bishop, 'b').
 letter(king,   'k').
 letter(knight, 'h').
@@ -58,6 +60,8 @@ letter(queen,  'q').
 letter(rook,   'r').
 letter("-", "_"). 
 
+
+% fixes Input given 
 fixInput(CurrX,CurrY,NextX,NextY):-
     enterMove(CX,CY,NX,NY),
     char_code(CX,CX1),
@@ -67,7 +71,7 @@ fixInput(CurrX,CurrY,NextX,NextY):-
     atom_number(CY,CurrY),
     atom_number(NY,NextY).
     
-   
+% enter moves takes in the coordinates and return true if the move is valid enterMove(CurrX,CurrY,NextX,NextY)
 enterMove(CurrX,CurrY,NextX,NextY):-
     repeat,
     write("\nEnter Player Location and move in form 'a1h8.' or enter 'quit.' to end the game \n"),   % will make prolog try till the answer is in the right form
@@ -81,7 +85,7 @@ enterMove(CurrX,CurrY,NextX,NextY):-
     p_input(CurrXUnprocessed,CurrYUnprocessed,NextXUnprocessed,NextYUnprocessed,CurrX,CurrY,NextX,NextY).
 
     
-% parsing input p_input (X,Y,X1,Y1,CX,CY,NX,NY) returns true if either quit or correct format is enterted    
+% p_input (X,Y,X1,Y1,CX,CY,NX,NY) parsing input given and returns true if either quit or correct format is enterted    
 p_input(CurrXUnprocessed,CurrYUnprocessed,NextXUnprocessed,NextYUnprocessed,CurrX,CurrY,NextX,NextY):-
     string_chars(CurrXUnprocessed,[CurrX]),
     string_chars(CurrYUnprocessed,[CurrY]),
@@ -102,7 +106,8 @@ p_input(CurrXUnprocessed,CurrYUnprocessed,NextXUnprocessed,NextYUnprocessed,Curr
     NextY = t,
     write("\n"),
     write("\n You have quit the game, let's play again next time (^.^)o[~] \n"), halt. 
-    
+
+% withinBounds(CurrX,CurrY,NextX,NextY),checks/ returns true if the coordinates given CurrX and CurrY and NextX and nextY are within bounds of the Board   
 withinBounds(CurrX,CurrY,NextX,NextY):-
     char_code(CurrX,VcurrX),
     char_code(NextX,VnextX),
@@ -117,6 +122,7 @@ withinBounds(CurrX,CurrY,NextX,NextY):-
     NextY < 9,
     NextY > -1.
 
+% playGame(C,Board):- take in the Color of the piece and Board, and plays until king is captured / does not exist on the board  
 playGame(w,Board):-
     \+member(piece(w,king,_,_),Board),
     write("\nBlACK WINS!"),
@@ -146,15 +152,18 @@ playGame(b,Board):-
     movePiece(Board,piece(b,Piece,X,Y),X1,Y1,Result),
     playGame(w,Result).
      
-    
+% Starts the Chess Game uses prediacte intiate2(Board) and playGame(w,Board), starts with white 
 start:-initiate2(Board),playGame(w,Board).
+
+% movePiece(Board,piece(X,Piece,X,Y),X1,Y1,Result) return true if we are able to move the piece to correct location on the Board based on X1 Y1 coord 
 movePiece(Board, piece(C,Piece,X,Y), X1,Y1,Result) :-
     Z is (X-1)+8*(Y-1),
     replace(Z,piece("-","-",X,Y),Board,Middle),
     Z2 is (X1-1)+8*(Y1-1),
     replace(Z2,piece(C,Piece,X1,Y1),Middle,Result).
     
-    
+
+% replaces the Elem in the list based on the Index    
 replace(_, _, [], []).
 replace(0,R,[H|T],[R|T]).
 replace(Index,R,[H1|T1],[H1|T2]) :-
