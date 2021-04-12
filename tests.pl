446b7e4f264de9b67f40a206823f7d68f5e72db1
kmr-source <59981595+kmr-source@users.noreply.github.com>
2021-04-11 18:35:49 -0700

    Update tests.pl
    fix errors in line 33

--- tests.pl
+++ tests.pl
@@ -8,6 +8,7 @@ knightTests:-
     knightTest(w,8,8,7,6).
     
 kingTest(C,X,Y,X1,Y1) :- initiate2(Board),king_helper(C,Board,X,Y,X1,Y1).
+
 kingTests:-
     \+kingTest(w,0,0,1,1),
     \+kingTest(w,0,0,-1,1),
@@ -17,11 +18,18 @@ kingTests:-
     
 testRook(Out):- initiate2(B),rookSteps(X),returnMoves(w,B,2,2,X,[],Out).
 testQueen(Out):- initiate2(B),queenSteps(X),returnMoves(w,B,2,2,X,[],Out).
-queenTest:-
+
+queenTest1:-
     initiate2(Board),
-    queen_helper(w,Board,2,2,5,5),
-    queen_helper(w,Board,2,2,7,7),
-    \+queen_helper(w,Board,2,2,1,1),
+    queen_helper(w,Board,2,2,5,5).
+    
+queenTest2:-
+    initiate2(Board),
+    queen_helper(w,Board,2,2,7,7).
+    
+queenTest3:- initiate2(Board),
+             queen_helper(w,Board,2,2,1,1).
+
 testBishop(Out):- initiate2(B),bishopSteps(X),returnMoves(w,B,2,2,X,[],Out).
 
 % TEST IT NOW PAWN 101
@@ -76,7 +84,7 @@ test_diagonal_startL:-
 % test out of Bounds
 outOfBounds:- initiate2(B),pawn_helper(w,B,-1,-1,9,1).
 outOfBounds2:-initate2(B),pawn_helper(b,B,-1,0,8,9).
-              
-             
-test(Z):-fixInput(X,Y,X1,Y1),
+
+% test fixInput(X,Y,X1,Y1)              
+testing(Z):-fixInput(X,Y,_,_),
     Z is (X-1)+8*(Y-1).
